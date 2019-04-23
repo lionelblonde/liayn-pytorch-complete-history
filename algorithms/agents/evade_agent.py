Correct erroneous spectral normalization

algorithms/agents/evade_agent.py
@@ -8,8 +8,6 @@ import torch.nn.utils as U
 import torch.nn.functional as F
 import torch.distributions as D
 
-from algorithms.helpers.spectral_normalization import SNLinear
-
 from algorithms.helpers import logger
 from algorithms.helpers.console_util import log_module_info
 from algorithms.helpers.distributed_util import average_gradients, sync_with_root
@@ -38,9 +36,7 @@ class Actor(nn.Module):
         self.ac_dim = self.ac_space.shape[0]
         self.hps = hps
 
-        # self.hid_fc_1 = nn.Linear(self.ob_dim, 300)
-
-        self.hid_fc_1 = SNLinear(self.ob_dim, 300)
+        self.hid_fc_1 = nn.Linear(self.ob_dim, 300)
 
         if self.hps.with_layernorm:
             self.layer_norm_1 = nn.LayerNorm(300)

algorithms/helpers/spectral_normalization.py
@@ -16,13 +16,13 @@ def normalize(v):
     return v / (torch.norm(v) + 1e-8)
 
 
-def normalize_spectrally(weight, u=None, power_iters=2):
+def normalize_spectrally(weight, u, v, power_iters=1):
     """Power iteration method for weight matrix `weights`"""
     assert power_iters > 0, "power method should iterate at least once"
     # Perform power method iterations
     for _ in range(power_iters):
-        v = normalize(torch.mv(weight.t(), u))
-        u = normalize(torch.mv(weight, v))
+        v.copy_(normalize(torch.mv(weight.t(), u)))
+        u.copy_(normalize(torch.mv(weight, v)))
     # Compute the spectral norm as u^T*W*v
     spectral_norm = torch.dot(u, torch.mv(weight, v))
     return spectral_norm, u
@@ -52,9 +52,11 @@ class SNLinear(nn.Linear):
     def __init__(self, in_features, out_features, bias=True):
         super(SNLinear, self).__init__(in_features, out_features, bias)
         self.register_buffer('u', torch.zeros(out_features).normal_(0, 1))
+        self.register_buffer('v', torch.zeros(in_features).normal_(0, 1))
 
     def forward(self, input):
         with torch.no_grad():
-            spectral_norm, u = normalize_spectrally(self.weight, self.u)
+            spectral_norm, u = normalize_spectrally(self.weight, self.u, self.v)
+            # print("self.u: {}".format(self.u))  # FIXME
             self.u.copy_(u)
         return F.linear(input, self.weight / spectral_norm, self.bias)