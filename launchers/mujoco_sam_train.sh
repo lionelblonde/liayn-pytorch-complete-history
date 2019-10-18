a884564ba0f4fad0cf9e39f9680d3e16e28398b5
lionelblonde <lionel.blonde@gmail.com>
2019-10-18 16:36:59 +0200

    Adjust hyperparameter in launcher file

--- launchers/mujoco_sam_train.sh
+++ launchers/mujoco_sam_train.sh
@@ -6,7 +6,7 @@ cd ..
 mpiexec -n $2 python main.py \
     --no-cuda \
     --env_id=$1 \
-    --seed=1 \
+    --seed=$5 \
     --checkpoint_dir="data/checkpoints" \
     --enable_visdom \
     --visdom_dir="data/summaries" \
@@ -17,7 +17,7 @@ mpiexec -n $2 python main.py \
     --num_iters=10000000 \
     --training_steps_per_iter=20 \
     --eval_steps_per_iter=50 \
-    --eval_frequency=10 \
+    --eval_frequency=100 \
     --prefill=200 \
     --no-render \
     --rollout_len=5 \
@@ -26,9 +26,9 @@ mpiexec -n $2 python main.py \
     --with_layernorm \
     --reward_scale=1. \
     --quantile_emb_dim=32 \
-    --num_tau=2 \
-    --num_tau_prime=2 \
-    --num_tau_tilde=4 \
+    --num_tau=4 \
+    --num_tau_prime=4 \
+    --num_tau_tilde=8 \
     --enable_clipped_double \
     --no-enable_targ_actor_smoothing \
     --actor_update_delay=2 \
@@ -42,13 +42,13 @@ mpiexec -n $2 python main.py \
     --noise_type="adaptive-param_0.2, ou_0.2" \
     --pn_adapt_frequency=10 \
     --gamma=0.99 \
-    --mem_size=1000000 \
+    --mem_size=50000 \
     --no-prioritized_replay \
     --alpha=0.3 \
     --beta=1. \
     --no-ranked \
     --no-unreal \
-    --wd_scale=1e-3 \
+    --wd_scale=1e-4 \
     --n_step_returns \
     --n=60 \
     --ent_reg_scale=0. \
